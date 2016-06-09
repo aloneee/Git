5045f12a3cf8e2c4994d56e6ec8f29c09de299be
liurihua <liurihua@bytedance.com>
2016-06-09 18:57:39 +0800

    Initial Commit

---
+++ Git/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Git
+//
+//  Created by Liu RiHua on 16/6/9.
+//  Copyright © 2016年 lrh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
